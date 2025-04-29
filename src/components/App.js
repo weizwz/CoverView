5eddbeabbbaa1d9586169c4d5f733414b422a4a1
weizwz <1124725517@qq.com>
2025-04-29 17:12:27 +0800

    删除无用页面

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Editor from './Editor';
-import Home from './Home'
 import Faq from './Faq';
 
 const App = () => {
@@ -10,8 +9,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/editor" element={<Editor />} />
+                <Route exact path="/" element={<Editor />} />
                 <Route exact path="/faq" element={<Faq />} />
             </Routes>
         </BrowserRouter>
